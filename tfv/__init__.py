0fa3dfa2b7460ac36ce1c5ca639dfaf9a69b207b
zihuig <1950565694@qq.com>
2021-07-11 15:23:34 +0800

    update __init__.py

--- tfv/__init__.py
+++ tfv/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import division
-from tfvsp.utils.quick_start import ModelTrain, FactChecking
+from tfv.main_file.TFV import TFV
 
 __version__ = '0.0.1'
