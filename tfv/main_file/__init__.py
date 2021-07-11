4d00b72623d211e8dcd2b840ac631bb084856a38
zihuig <1950565694@qq.com>
2021-07-11 15:22:16 +0800

    update __init__.py

--- tfv/main_file/__init__.py
+++ tfv/main_file/__init__.py
@@ -1,4 +1,1 @@
-from tfvsp.main_file.sat_main_mlm_cell import mlm_run
-from tfvsp.main_file.sat_sample import run
-from tfvsp.main_file.sat_sample_nn import nn_run
-from tfvsp.main_file.tfvsp import fact_checking
+from tfv.main_file.TFV import TFV
